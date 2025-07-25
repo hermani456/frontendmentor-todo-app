c606ed9e906f99e30b3d71fe81da2221914bc0ea
diego <diego.campuzano@sellside.cl>
2025-07-25 17:29:23 -0400

    feat: add initial setup for a React app with dark/light mode toggle
    - Added background images for desktop and mobile in dark and light modes.
    - Included favicon and various SVG icons for UI elements.
    - Implemented main App component with dark mode functionality using useState.
    - Set up Tailwind CSS for styling and responsive design.
    - Created TypeScript configuration files for app and node environments.
    - Configured Vite as the build tool with React and Tailwind CSS plugins.

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import moon from "./assets/icon-moon.svg";
+import sun from "./assets/icon-sun.svg";
+import { useState } from "react";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  return (
+    <div className={`min-h-screen flex flex-col ${darkMode ? "dark" : ""}`}>
+      <div
+        className={`relative w-full min-h-[400px] bg-cover bg-center bg-no-repeat ${
+          darkMode
+            ? "bg-[url('./assets/bg-desktop-dark.jpg')]"
+            : "bg-[url('./assets/bg-desktop-light.jpg')]"
+        }`}
+      >
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="max-w-xl w-full mx-auto p-8 flex items-center justify-between">
+            <h1 className="text-white text-4xl font-bold tracking-[1rem]">
+              TODO
+            </h1>
+            <button onClick={() => setDarkMode(!darkMode)}>
+              {darkMode ? (
+                <img src={sun} alt="Toggle Light Mode" className="w-7" />
+              ) : (
+                <img src={moon} alt="Toggle Dark Mode" className="w-7" />
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 bg-white dark:bg-gray-800">
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
